0c597e63160ca5852df88cadbb8ccd05d70c94ef
aditya <aditya7238195@gmail.com>
2021-11-05 13:19:46 +0530

    :art: UI improved for leader board page

--- src/Components/LeaderBoard/LeaderBoard.component.jsx
+++ src/Components/LeaderBoard/LeaderBoard.component.jsx
@@ -1,17 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import SportsScoreIcon from "@mui/icons-material/SportsScore";
 import "./LeaderBoard.styles.css";
 const LeaderBoard = () => {
-  const [type, setType] = useState("coverage");
+  const [type, setType] = useState("likes");
   const [tableData, setTableData] = useState([
-    { name: "aditya", coverage: 50, likes: 40 },
-    { name: "aditya", coverage: 510, likes: 40},
-    { name: "aditya", coverage: 530, likes: 4320 },
-    { name: "aditya", coverage: 5102, likes: 420 },
-    { name: "aditya", coverage: 540, likes: 402 },
-    { name: "aditya", coverage: 5120, likes: 450 },
-    { name: "aditya", coverage: 5450, likes: 410 },
-    { name: "aditya", coverage: 520, likes: 406 },
-  ])
+    { name: "aditya", coverage: 54, likes: 410 },
+    { name: "aditya", coverage: 51, likes: 450 },
+    { name: "aditya", coverage: 51, likes: 420 },
+    { name: "aditya", coverage: 40, likes: 402 },
+    { name: "aditya", coverage: 30, likes: 4320 },
+    { name: "aditya", coverage: 20, likes: 406 },
+    { name: "aditya", coverage: 10, likes: 40 },
+    { name: "aditya", coverage: 5, likes: 40 },
+  ]);
   const GetSortOrder = () => {
     return function (a, b) {
       if (a[type] < b[type]) {
@@ -22,61 +23,99 @@ const LeaderBoard = () => {
       return 0;
     };
   };
-  const displayRows = () =>{
-      return tableData.map((row) => {
-                return (
-                  <tr className="leader-baord-row">
-                    <td className="row-tiles">{row.name}</td>
-                    <td className="row-tiles">{row.coverage}</td>
-                    <td className="row-tiles">{row.likes}</td>
-                  </tr>
-                );
-              })
-  }
+
   const setByType = () => {
     console.log(type);
     const data = tableData.sort(GetSortOrder());
     return data;
   };
-  useEffect(() => {
-    setTableData(setByType());
-    console.log(tableData)
-  }, [type]);
-  const data = [
-    { name: "aditya", coverage: 50, likes: 40 },
-    { name: "aditya", coverage: 510, likes: 40},
-    { name: "aditya", coverage: 530, likes: 4320 },
-    { name: "aditya", coverage: 5102, likes: 420 },
-    { name: "aditya", coverage: 540, likes: 402 },
-    { name: "aditya", coverage: 5120, likes: 450 },
-    { name: "aditya", coverage: 5450, likes: 410 },
-    { name: "aditya", coverage: 520, likes: 406 },
-  ];
+
   return (
     <div style={{ marginTop: "100px" }} className="leader-board-container">
-      <div className="leader-baord-title">LeaderBoard</div>
+      <div className="leader-board-title">
+        <p>Leader Board</p>
+        <SportsScoreIcon sx={{ fontSize: 30 }} />
+      </div>
       <div className="leader-board-option">
-        <span className="option-tab">
-          Order By:
-          <span onClick={() => setType("likes")}>Coverage</span>
-          <span onClick={() => setType("coverage")}>Likes</span>
+        <span className="option-tiles">Order By:</span>
+        <span
+          className={
+            type === "coverage"
+              ? "option-tiles option-click "
+              : "option-tiles option-click active-order"
+          }
+          onClick={() => {
+            setType("likes");
+            setTableData(setByType());
+          }}
+        >
+          Coverage
+        </span>
+        <span
+          className={
+            type === "likes"
+              ? "option-tiles option-click "
+              : "option-tiles option-click active-order"
+          }
+          onClick={() => {
+            setType("coverage");
+            setTableData(setByType());
+          }}
+        >
+          Likes
         </span>
       </div>
       <div className="leader-board-data">
         <table className="leader-board-table">
           <tr>
-          <th>Rank</th>
+            <th>Rank</th>
             <th>Name</th>
-            <th>Coverage</th>
-            <th>Likes</th>
+            <th>Coverage (%)</th>
+            <th>Likes (#)</th>
           </tr>
-          {tableData.map((row,i) => {
+          {tableData.map((row, i) => {
             return (
-              <tr className="leader-baord-row">
-              <td>{i+1}</td>
-                <td className="row-tiles">{row.name}</td>
-                <td className="row-tiles">{row.coverage}</td>
-                <td className="row-tiles">{row.likes}</td>
+              <tr className="leader-board-row">
+                <td
+                  className="row-tiles"
+                  style={
+                    i % 2 === 0
+                      ? { backgroundColor: "#ff3f6c", color: "white" }
+                      : { backgroundColor: "white" }
+                  }
+                >
+                  {i + 1}
+                </td>
+                <td
+                  className="row-tiles"
+                  style={
+                    i % 2 === 0
+                      ? { backgroundColor: "#ff3f6c", color: "white" }
+                      : { backgroundColor: "white" }
+                  }
+                >
+                  {row.name}
+                </td>
+                <td
+                  className="row-tiles"
+                  style={
+                    i % 2 === 0
+                      ? { backgroundColor: "#ff3f6c", color: "white" }
+                      : { backgroundColor: "white" }
+                  }
+                >
+                  {row.coverage}
+                </td>
+                <td
+                  className="row-tiles"
+                  style={
+                    i % 2 === 0
+                      ? { backgroundColor: "#ff3f6c" }
+                      : { backgroundColor: "white" }
+                  }
+                >
+                  {row.likes}
+                </td>
               </tr>
             );
           })}
